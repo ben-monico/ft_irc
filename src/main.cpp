f8e860b9620cf18532ef831a392055b9c800c53c
Bernardo <bcmonico@gmail.com>
2023-05-23 01:03:07 +0100

    fixed some sht, understood some more

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bcarreir <bcarreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 23:58:54 by bcarreir          #+#    #+#             */
-/*   Updated: 2023/05/22 19:26:05 by bcarreir         ###   ########.fr       */
+/*   Updated: 2023/05/23 01:02:46 by bcarreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,16 +36,15 @@ int main(int ac, char **av)
 	(void) av;
 	struct addrinfo hints, *servinfo, *aux;
 	struct sigaction sa;
-	int sockfd, new_fd, yes = 1;
-
+	
 	ft_bzero(&hints, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE; //for my IP
 
-	int ret;
+	int ret, sockfd, yes = 1;
 	std::string err = "Server error: getaddrinfo() - ";
-	if ((ret = getaddrinfo("0.0.0.0", PORT, &hints, &servinfo)))
+	if ((ret = getaddrinfo(NULL, PORT, &hints, &servinfo)))
 	{
 		err += gai_strerror(ret);
 		err_out(err);
@@ -69,45 +68,55 @@ int main(int ac, char **av)
 		break ;
 	}
 	freeaddrinfo(servinfo);
+	servinfo = NULL;
+	
+	//Couldn't find a valid address
 	if (!aux)
 		err_out("Server error: unable to bind.");
+	//Unable to listen to socketfd
 	if (listen(sockfd, BACKLOG) == -1)
 		err_out("Server error: listen()");
+	//Reap all dead processes
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
 	if (sigaction(SIGCHLD, &sa, NULL) == -1)
 		err_out("Server error: sigaction()");
+	
 	// char hostname[256];
     // if (gethostname(hostname, sizeof(hostname)) == 0)
 	// 	std::cout << "Server hostname: " << hostname << std::endl;
+	
 	std::cout << "Waiting for connections..." << std::endl;
+	
 	socklen_t sin_size;
 	struct sockaddr_storage user_addr;
 	char s[INET_ADDRSTRLEN];
-	int sendn;
+	int sent_size, accept_fd;
 	while (1)
 	{
 		sin_size = sizeof(user_addr);
-		new_fd = accept(sockfd, (struct sockaddr *)&user_addr, &sin_size);
-		if (new_fd == -1)
+		accept_fd = accept(sockfd, (struct sockaddr *)&user_addr, &sin_size);
+		if (accept_fd == -1)
 		{
-			std::cout << "Server error: accept()." << std::endl;
+			std::cerr << "Server error: accept()." << std::endl;
 			continue;
 		}
 		inet_ntop(user_addr.ss_family, get_in_addr((struct sockaddr *)&user_addr), s, sizeof(s));
 		std::cout << "Server: Successful connection from " << s << std::endl;
 		
-		//Client not receiving messages
+		//Working with telnet, but not with Hexchat
+		//Might have to do with IRC protocol message format
 		std::string str;
 		std::cout << "Send message: ";
 		std::getline(std::cin, str);
-		sendn = send(new_fd, str.c_str(), str.size(), 0);
-		if (sendn == -1)
+		sent_size = send(accept_fd, str.c_str(), str.size(), 0);
+		if (sent_size == -1)
 		{
 			std::cerr << "Server error: send()" << std::endl;
-			close(new_fd);
+			close(accept_fd);
 			continue;
 		}
-		std::cout << "Message Sent. Number of bytes: "<< sendn << std::endl;
+		std::cout << "Message Sent. Number of bytes: "<< sent_size << std::endl;
 	}
 }
+
