efae6369d062eae303634f10d98eae65f6a078cf
Bernardo <bcmonico@gmail.com>
2023-06-06 16:58:30 +0100

    dealing with chanops

--- src/Context_chanops.cpp
+++ src/Context_chanops.cpp
@@ -8,27 +8,33 @@ void Context::chanop_kick(std::string const& channel, int client_id)
 	//:kickerNick!~kickerUserName@hostname KICK #channel targetuser :You have been kicked by kickuser: Reason for kick.
 	std::vector<Channel>::iterator it = find_chan_by_name(channel);
 	if (it != _channels.end())
-	{
 		client->eraseChannel(channel);
-	}
 	server->sendAllBytes(client->getNick() + " KICK #" + channel + " " + client->getNick() + \
 	 " :You have been kicked by " + client->getNick() + "\r\n", client->getId());
 }
 
 void Context::chanop_invite(int client_id, std::string channel)
 {
-	std::vector<Client>::iterator client = find_client_by_id(client_id);
 	std::vector<Channel>::iterator it = find_chan_by_name(channel);
-	if (client->getChannelMode(channel) == "o" && it != _channels.end())
+	if (it != _channels.end())
 		it->toggleInviteOnly();
+	server->sendAllBytes(":" + client->getNick() + " MODE #" + channel + " +i\r\n", client->getId());
 }
 
 void Context::chanop_topic(int client_id, std::string const& channel)
 {
-	std::vector<Client>::iterator client = find_client_by_id(client_id);
 	std::vector<Channel>::iterator it = find_chan_by_name(channel);
-	if (client->getChannelMode(channel) == "o" && it != _channels.end())
+	if (it != _channels.end())
 		it->toggleRestrictTopic();
+	server->sendAllBytes(":" + client->getNick() + " MODE #" + channel + " +t\r\n", client->getId());
+}
+
+void Context::chanop_userlimit(int client_id, std::string const& channel, std::string const& userLimit)
+{
+	std::vector<Channel>::iterator it = find_chan_by_name(channel);
+	if (it != _channels.end())
+		it->setUserLimit(userLimit);
+	server->sendAllBytes(":" + client->getNick() + " MODE #" + channel + " +l " + userLimit + "\r\n", client_id);
 }
 
 void Context::chanop_mode(int client_id, std::string const& channel, std::string const& modes) 
