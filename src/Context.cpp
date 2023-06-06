905d62b6dec6fb3d5e5f6d6a9e326e210e3eafd2
Leonardo osvaldo Ferreira mainini <leferrei@student.42lisboa.com>
2023-06-06 18:36:55 +0100

    Revert "merge login" - fuck my branch so hard
    This reverts commit f30e40a45538c82083f8dad0feb5774b0c0e123e, reversing
    changes made to 893e94bfce2a5aada9f83ce054ccd438b776e053.

--- src/Context.cpp
+++ src/Context.cpp
@@ -2,13 +2,11 @@
 #include <Channel.hpp>
 #include <Handler.hpp>
 #include <sstream>
-//ERRORS ON PAGE 43
 
 std::vector<Channel> Context::_channels;
 std::vector<Client> Context::_clients;
-std::string Context::welcome = "Welcome to ft_irc\r\n";
 Handler *Context::server = 0;
-std::string Context::_hostname = ":localhost";
+std::string Context::_hostname = ":localhost ";
 
 void	Context::setServerPtr( Handler *serverPtr )
 {
@@ -24,7 +22,7 @@ std::vector<Channel>::iterator	Context::find_chan_by_name(std::string name)
 	return it;	
 }
 
-std::vector<Client>::iterator  Context::find_client_by_id(int id)
+std::vector<Client>::iterator Context::find_client_by_id(int id)
 {
 	std::vector<Client>::iterator it = Context::_clients.begin();
 	for (; it != _clients.end(); ++it)
@@ -40,113 +38,6 @@ std::vector<Client>::iterator  Context::find_client_by_nick(const std::string &n
 		if (it->getNick() == nick)
 			break;
 	return it;	
-}
-
-Client	&Context::get_client_by_id(int id)
-{
-	std::vector<Client>::iterator it = Context::_clients.begin();
-	for (; it != _clients.end(); ++it)
-		if (it->getId() == id)
-			break;
-	return *it;	
-}
-
-//Commands
-void Context::cmd_join(int client_id, std::string const &channel)
-{
-	std::vector<Client>::iterator client = find_client_by_id(client_id);
-	//check if invite only, or for pwd
-	std::vector<Channel>::iterator it = find_chan_by_name(channel);
-	if (it == _channels.end())
-	{
-		_channels.push_back(Channel(channel, 'o'));
-		it = (_channels.end())--;
-		//missing setMode @
-	}
-	//how to set client mode?
-	it->addClient(*client);
-	client->setMode(channel, "");
-	RPL_TOPIC(client->getId(), *it);
-	RPL_NAMREPLY(client->getId(), *it);
-	RPL_ENDOFNAMES(client->getId(), *it);
-
-	// /ERR_NEEDMOREPARAMS              ERR_BANNEDFROMCHAN
-	// ERR_INVITEONLYCHAN              ERR_BADCHANNELKEY
-	// ERR_CHANNELISFULL               ERR_BADCHANMASK
-	// ERR_NOSUCHCHANNEL               ERR_TOOMANYCHANNELS
-	// RPL_TOPIC
-}
-
-void Context::cmd_setNick(int client_id, std::string nick)
-{
-	std::vector<Client>::iterator client = find_client_by_id(client_id);
-	//NICK | ERR_NICKCOLLISION ERR_NONICKNAMEGIVEN ERR_NICKNAMEINUSE ERR_ERRONEUSNICKNAME
-	client->setNick(nick);
-}
-
-void Context::cmd_setUserName(int client_id, std::string userName)
-{
-	std::vector<Client>::iterator client = find_client_by_id(client_id);
-	client->setUserName(userName);
-}
-
-void Context::cmd_sendPM(int client_id, std::string const & msg)
-{
-	std::vector<Client>::iterator client = find_client_by_id(client_id);
-	//get target in message, send() desired PM
-	(void)client;
-	(void)msg;
-	// ERR_NORECIPIENT                 ERR_NOTEXTTOSEND
-	//    ERR_CANNOTSENDTOCHAN            ERR_NOTOPLEVEL
-	//    ERR_WILDTOPLEVEL                ERR_TOOMANYTARGETS
-	//    ERR_NOSUCHNICK
-	//    RPL_AWAY
-}
-
-//Channel operations
-void Context::chanop_kick(std::string const & channel, int client_id)
-{
-	std::vector<Client>::iterator client = find_client_by_id(client_id);
-	//:kickerNick!~kickerUserName@hostname KICK #channel targetuser :You have been kicked by kickuser: Reason for kick.
-	std::vector<Channel>::iterator it = find_chan_by_name(channel);
-	if (it != _channels.end())
-	{
-		(*it).removeClientFromChannel(client->getNick());
-		client->eraseChannel(channel);
-	}
-	server->sendAllBytes(client->getNick() + " KICK #" + channel + " " + client->getNick() + \
-	 " :You have been kicked by " + client->getNick() + "\r\n", client->getId());
-}
-
-void Context::chanop_invite(int client_id, std::string channel)
-{
-	std::vector<Client>::iterator client = find_client_by_id(client_id);
-	//if channel is invite only mode, invite must be chanop
-	//ERR_NEEDMOREPARAMS              ERR_NOSUCHNICK
-	//    ERR_NOTONCHANNEL                ERR_USERONCHANNEL
-	//    ERR_CHANOPRIVSNEEDED
-	//    RPL_INVITING                    RPL_AWAY
-	(void)client;
-	(void)channel;
-}
-
-void Context::chanop_topic(int client_id, std::string topic)
-{
-	std::vector<Client>::iterator client = find_client_by_id(client_id);
-	(void)client;
-	(void)topic;
-	// ERR_NEEDMOREPARAMS              ERR_NOTONCHANNEL
-	//    RPL_NOTOPIC                     RPL_TOPIC
-	//    ERR_CHANOPRIVSNEEDED
-}
-
-
-void Context::chanop_mode(Channel &channel, char c, std::string const & msg) 
-{
-	//check msg in case of additional params
-	if (c == 'i' || c == 't' || c == 'k' || c == 'o' || c == 'l')
-		channel.setMode(c);
-	(void) msg;
 }
 
 void	Context::add_client(int client_id)
@@ -165,7 +56,7 @@ void Context::addClientToChannel(int client_id, std::string const & channelname,
 {
 	std::vector<Client>::iterator it = find_client_by_id(client_id);
 	if (it  != _clients.end())
-		it->setMode(channelname, mode);
+		it->addChannelMode(channelname, mode);
 }
 
 void Context::removeClientFromChannel(int client_id, std::string const & channelname)
@@ -173,25 +64,6 @@ void Context::removeClientFromChannel(int client_id, std::string const & channel
 	std::vector<Client>::iterator it = find_client_by_id(client_id);
 	if (it  != _clients.end())
 		it->eraseChannel(channelname);
-}
-
-
-//Command responses
-void	Context::execClientCmds(int id)
-{
-	std::vector<Client>::iterator		client = find_client_by_id(id); 
-	std::vector<std::string>			&cmds = client->getCmds();
-	std::vector<std::string>::iterator	it = cmds.begin();
-
-	
-	for (; it != cmds.end(); ++it)
-	{
-		if (it->find("QUIT") != std::string::npos)
-		{
-			server->closeConection(client->getId());
-			break ;
-		}
-	}		
 }
 
 void	Context::verifyLoginInfo(int id)
@@ -215,18 +87,20 @@ void	Context::verifyLoginInfo(int id)
 		std::getline(username, user, ' ');
 	if (pass.empty() || !server->isPasswordMatch(pass))
 	{
-		server->sendAllBytes(_hostname + " 464 " + nick + " :Password incorrect\r\n", id);
+		ERR_PASSWDMISMATCH(id);
 		server->closeConection(id);
 	}
 	else if (isUserInVector(find_client_by_nick(nick)))
 	{
-		//:irc.server.com 433 YourNickname :Nickname is already in use.
-		server->sendAllBytes(_hostname + " 422 " + nick + " :Nickname is already in use.\r\n", id);
+		USR_RPL_TEMPLATE("436", "Nickname is already in use.", nick, id);
 		// server->closeConection(id);
 	}
 	else
 	{
-		server->sendAllBytes(_hostname + " 001 " + nick + " :Welcome to FT_IRC!\r\n", id);
 		client->init(nick, user);
+		//need to go over CAP again
+		Context::RPL_WELCOME(client->getId());
 	}
 }
+
+bool Context::isUserInVector(std::vector<Client>::iterator userGot) { return (userGot != _clients.end()); }
