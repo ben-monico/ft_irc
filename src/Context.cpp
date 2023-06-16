7cffd052d2abd530e0a75f10fb260121631322e1
Bernardo <bcmonico@gmail.com>
2023-06-16 15:16:50 +0100

    added broadcast notify when a user quits the serv

--- src/Context.cpp
+++ src/Context.cpp
@@ -137,7 +137,11 @@ void Context::removeClientFromAllChannels(std::vector<Client>::iterator const & 
 		std::map<std::string, std::string> channels = client->getChannels();
 		std::map<std::string, std::string>::iterator it = channels.begin();
 		for (; it != channels.end(); ++it)
-			removeClientFromChannel(client, findChannelByName(it->first));
+		{
+			std::vector<Channel>::iterator channel = findChannelByName(it->first);
+			channel->broadcastMsg(":" + client->getNick() + " PART #" + channel->getName() + " :" + client->getNick() + " has quit the server\r\n", server, -1);
+			removeClientFromChannel(client, channel);
+		}
 	}
 }
 
